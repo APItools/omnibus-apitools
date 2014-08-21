877267f290fb68c5eaeb783bf465bdeed6e4914b
Michal Cichra <michal@o2h.cz>
2014-08-21 17:51:22 +0200

    release 201408211748

--- config/software/apitools-release.rb
+++ config/software/apitools-release.rb
@@ -1,5 +1,5 @@
 name 'apitools-release'
-default_version '201408041328'
+default_version '201408211748'
 
 dependency 'apitools-lua'
 # dependency 'supervisor'
@@ -7,7 +7,7 @@ dependency 'apitools-lua'
 relative_path "monitor-#{version}"
 
 source :url => "https://github.com/APItools/monitor/archive/#{version}.tar.gz",
-       :md5 => "4171f68b30572cbc9ce0506d206e2a64"
+       :md5 => "8cdcd8768dc4c20c7df6ba09e48c6155"
 
 build do
   command ["cp -r . #{install_dir}/release" ]
